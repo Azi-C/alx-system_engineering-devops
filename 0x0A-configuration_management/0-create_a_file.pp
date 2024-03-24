9bb314820f9e92791af2257a9a65867276190867
Azi-C <aaziza.chbihi@gmail.com>
2024-03-24 16:49:11 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This file will createe a new file using Puppet
 
 file { '/tmp/school':
-	ensure	=> file,
-	content	=> 'I love Puppet',
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',	
+	ensure	=> 	file,
+	content	=> 	'I love Puppet',
+	mode	=> 	'0744',
+	owner	=>	'www-data',
+	group	=>	'www-data',	
 }
